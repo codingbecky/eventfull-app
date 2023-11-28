b9da1e94593f02a040f09a4c586a67d57fc50555
DESKTOP-5LSL4LR\SiAmComE <siamcomdeveloper@gmail.com>
2023-11-28 02:40:27 -0800

    Add a jwt helper

---
+++ src/helper/jwt.helper.js
@@ -0,0 +1,11 @@
+export const getJwtToken = () => {
+    return localStorage.getItem('eventfulluser');
+}
+
+export const saveJwtToken = (jwtToken) => {
+    return localStorage.setItem('eventfulluser', jwtToken);
+}
+
+export const removeJwtToken = () => {
+    return localStorage.removeItem('eventfulluser');
+}
